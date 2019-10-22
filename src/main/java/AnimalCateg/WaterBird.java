9996952e526eb71b7eb95d4e4e93872c95139edc
Alberto Martinez <alberto.martinez@gmail.com>
2019-10-22 03:31:15 +0200

    Add WaterBird subClass

---
+++ src/main/java/AnimalCateg/WaterBird.java
@@ -0,0 +1,21 @@
+package AnimalCateg;
+
+import Exercise1.Swimmer;
+
+public class WaterBird extends Animal implements Swimmer {
+
+    @Override
+    public void setType(String type) {
+        super.setYear(type);
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setYear(name);
+    }
+
+    @Override
+    public void setYear(String year) {
+        super.setYear(year);
+    }
+}
